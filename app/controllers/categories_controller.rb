6104d0439e4f365cbcff49aee06a9e3fd075df19
Muhammet Ali YILDIZ <muhammetali_969@hotmail.com>
2025-02-11 19:37:28 +0300

    .

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,17 +4,34 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to  categories_path, notice: "basarili"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @category.update(category_params)
+      redirect_to categories_pathm, notice: "Guncellendi"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
   end
+
+  private
+    def category_params
+        params.expect(category: [ :title ])
+    end
 end
